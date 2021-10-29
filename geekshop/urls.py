d2006db3edbc0a112bf509fa87a3f753fe0489a3
Xemur0 <aleks.prfnk@gmail.com>
2021-10-29 17:07:28 +0300

    try to fix

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
+from django.template.defaulttags import url
 from django.urls import path
 import mainapp.views as mainapp
 from django.conf import settings
@@ -32,9 +34,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    ]
 
 
