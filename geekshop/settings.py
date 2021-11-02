8c9ca8fb20c20bda8434a1135e6ec492ebd98950
Xemur0 <aleks.prfnk@gmail.com>
2021-11-02 17:30:35 +0300

    DB opti

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 # STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
@@ -156,7 +156,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
