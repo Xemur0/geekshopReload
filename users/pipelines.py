2636205da5233b72a81272c51893111c7f0a8917
Xemur0 <aleks.prfnk@gmail.com>
2021-11-13 18:37:24 +0300

    almost finished

--- users/pipelines.py
+++ users/pipelines.py
@@ -14,7 +14,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         return
 
     api_url = urlunparse(('http', 'api.vk.com', '/method/users.get', None, urlencode(
-        OrderedDict(fields=','.join(('bdate', 'sex', 'about')), access_token=response['access_token'],
+        OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_200')), access_token=response['access_token'],
                     v=5.131)), None))
 
     resp = requests.get((api_url))
@@ -23,13 +23,15 @@ def save_user_profile(backend, user, response, *args, **kwargs):
 
     data = resp.json()['response'][0]
 
+    if data['photo_200']:
+        photo_link = data['photo_200']
+        photo_response = requests.get(photo_link)
+        path_photo = f'users_image/{user.pk}.jpg'
+        with open(f'media/{path_photo}', 'wb') as photo:
+            photo.write(photo_response.content)
+        user.image = path_photo
 
-
-    if backend.name == 'vk-oauth2':
-        url = response.get('photo', '')
-
-    if url:
-        user.image = url
+    user.save()
 
 
 
--- users/views.py
+++ users/views.py
@@ -97,22 +97,3 @@ class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
 
 class Logout(LogoutView):
     template_name = 'mainapp/index.html'
-
-# def send_verify_link(user):
-#     verify_link = reverse('users:verify',args=[user.email,user.activation_key])
-#     subject = f'Для активации учетной записи {user.username} пройдите по ссылке'
-#     message = f'Для подтверждения учетной записи {user.username} на портале \n {settings.DOMAIN_NAME}{verify_link}'
-#     return send_mail(subject,message,settings.EMAIL_HOST_USER,[user.email],fail_silently=False)
-#
-# def verify(request,email,activation_key):
-#     try:
-#         user = User.objects.get(email=email)
-#         if user and user.activation_key == activation_key and not user.is_activation_key_expired():
-#             user.activation_key = ''
-#             user.activation_key_created = None
-#             user.is_active = True
-#             user.save()
-#             auth.login(request,user, backend='django.contrib.auth.backends.ModelBackend')
-#         return render(request,'users/verification.html')
-#     except Exception as e:
-#         return HttpResponseRedirect(reverse('index'))
